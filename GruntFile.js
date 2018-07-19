e69ea13c2c550c2ea5bbd54f7c38ec397a25569f
kampkelly <kampkellykeys@gmail.com>
2018-07-19 12:34:24 +0100

    Add grunt tasks for eslint and mocha

--- GruntFile.js
+++ GruntFile.js
@@ -2,19 +2,32 @@ module.exports = function(grunt) {
     require("load-grunt-tasks")(grunt); // npm install --save-dev load-grunt-tasks
     grunt.initConfig({
       "babel": {
-        options: {
-          sourceMap: true
-        },
-        dist: {
-			files: [{
-		      expand: true,
-		      cwd: 'src',
-		      src: ['**/*.js'],
-		      dest: 'app/dist',
-		      ext: '.js'
-		  }]
-        }
-       }
+	        options: {
+	          sourceMap: true
+	        },
+	        dist: {
+				files: [{
+			      expand: true,
+			      cwd: 'src',
+			      src: ['**/*.js'],
+			      dest: 'app/dist',
+			      ext: '.js'
+			  }]
+	        }
+		},
+		eslint: {
+            target: ['src/**/*.js']
+       	},
+	   	mochaTest: {
+		  test: {
+		    options: {
+		      reporter: 'spec',
+		      require: 'babel-register'
+		    },
+		    src: ['app/dist/test/**/*.js']
+		    // src: ['app/dist/test/users-spec.js', 'app/dist/test/entries-spec.js']
+		  }
+	   	}
     });
 
     grunt.registerTask("default", ["babel"]);
