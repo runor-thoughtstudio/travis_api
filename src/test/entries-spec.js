079f63ed77b9e2f85931f8c2adf999bb39ac0419
kampkelly <kampkellykeys@gmail.com>
2018-07-19 15:29:56 +0100

    Add test for showing all entries
    This writes the test for getting all entries.

--- src/test/entries-spec.js
+++ src/test/entries-spec.js
@@ -6,3 +6,17 @@ import Request from '../lib/requests';
 dotenv.config();
 const { expect } = chai;
 const request = new Request();
+
+describe('Test Entries Routes', () => {
+	describe('allEntries()', () => {
+		it('should show all entries in the app', (done) => {
+			const url = `${process.env.root_url}/${process.env.version_url}/entries`;
+			request.get(url, (error, res, body) => {
+				const jsonObject = JSON.parse(body);
+				expect(res.statusCode).to.be.equal(200);
+				expect(jsonObject).to.be.a('array');
+				done();
+			});
+		}).timeout(10000);
+	});
+});
