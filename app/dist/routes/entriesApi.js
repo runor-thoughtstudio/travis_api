44abc26c16d77056b34a71cb4e3b1e3e5731912a
kampkelly <kampkellykeys@gmail.com>
2018-07-19 15:22:35 +0100

    Create the all entries api route

--- app/dist/routes/entriesApi.js
+++ app/dist/routes/entriesApi.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+	value: true
 });
 
 var _express = require('express');
@@ -11,6 +11,14 @@ var _express2 = _interopRequireDefault(_express);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var entriesRouter = _express2.default.Router();
+
+entriesRouter.get('/entries', function (req, res) {
+	var datastructure = req.app.get('appData');
+	if (!datastructure.entries) {
+		res.status(404).json({ error: 'Not Found' });
+	}
+	res.status(200).json(datastructure.entries);
+});
 
 exports.default = entriesRouter;
 //# sourceMappingURL=entriesApi.js.map
--- src/routes/entriesApi.js
+++ src/routes/entriesApi.js
@@ -2,4 +2,12 @@ import express from 'express';
 
 const entriesRouter = express.Router();
 
+entriesRouter.get('/entries', (req, res) => {
+	const datastructure = req.app.get('appData');
+	if (!datastructure.entries) {
+		res.status(404).json({ error: 'Not Found' });
+	}
+	res.status(200).json(datastructure.entries);
+});
+
 export default entriesRouter;
