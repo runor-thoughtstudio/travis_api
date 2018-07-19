742f4cd23bc91e26d30442fbe131c58971842344
kampkelly <kampkellykeys@gmail.com>
2018-07-19 22:53:45 +0100

    Update create entry test

--- src/test/entries-spec.js
+++ src/test/entries-spec.js
@@ -69,6 +69,7 @@ describe('Test Entries Routes', () => {
 				const jsonObject = JSON.parse(body);
 				expect(res.statusCode).to.be.equal(201);
 				expect(jsonObject).to.be.a('object');
+				expect(jsonObject.message).to.be.equal('The entry has been created!');
 				done();
 			});
 		}).timeout(10000);
