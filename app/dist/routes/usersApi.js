ae95b9f669d153a424e01f3aea27cb47f4389e59
kampkelly <kampkellykeys@gmail.com>
2018-07-19 13:26:58 +0100

    Initialize users api route

--- app/dist/routes/usersApi.js
+++ app/dist/routes/usersApi.js
@@ -1,2 +1,16 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var usersRouter = _express2.default.Router();
+
+exports.default = usersRouter;
 //# sourceMappingURL=usersApi.js.map
