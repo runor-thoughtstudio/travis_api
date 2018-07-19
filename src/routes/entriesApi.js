e7344f66adb4d4d437d6567b2dd62735daa0e9a9
kampkelly <kampkellykeys@gmail.com>
2018-07-19 22:44:23 +0100

    Add api routes to create an entry

--- src/routes/entriesApi.js
+++ src/routes/entriesApi.js
@@ -14,7 +14,7 @@ entriesRouter.get('/entries', (req, res) => {
 entriesRouter.get('/entries/:id', (req, res) => {
 	const datastructure = req.app.get('appData');
 	if (!datastructure.entries) {
-		res.status(500).json({ error: 'Internal Server Error' });
+		res.status(500).json({ error: 'Internal Server Error!' });
 	} else if (!Number.isInteger(Number(req.params.id))) {
 		res.status(400).json({ error: 'Bad Request!' });
 	} else if
@@ -23,6 +23,20 @@ entriesRouter.get('/entries/:id', (req, res) => {
 	} else {
 		const entry = datastructure.entries[req.params.id];
 		res.status(200).json(entry);
+	}
+});
+
+entriesRouter.post('/entries', (req, res) => {
+	const datastructure = req.app.get('appData');
+	if (req.body.title === ' ' || req.body.description === ' ' || req.body.title.length < 1 || req.body.description < 1) {
+		res.status(422).json({ message: 'Please fill in all the fields properly!' });
+	} else if (!datastructure.entries) {
+		res.status(500).json({ error: 'Internal Server Error!' });
+	} else if (req.body.title && req.body.description) {
+		datastructure.entries.push(req.body);
+		res.status(201).json({ message: 'The entry has been created!' });
+	} else {
+		res.status(400).json({ message: 'Invalid request!' });
 	}
 });
 
