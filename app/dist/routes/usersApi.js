db1f803c83844322f2dcad37085ce0957e9eeea3
kampkelly <kampkellykeys@gmail.com>
2018-07-20 00:42:03 +0100

    Refactor signup api for a user

--- app/dist/routes/usersApi.js
+++ app/dist/routes/usersApi.js
@@ -24,28 +24,49 @@ usersRouter.post('/users', function (req, res) {
 	    dob = _req$body.dob,
 	    fullName = _req$body.fullName;
 
-	if (email && dob && fullName && password) {
-		if (email.length > 0 && dob.length > 0 && fullName.length > 0 && password.length >= 6) {
-			var user = datastructure.users.filter(function (u) {
-				return u.email === email && u.password === password;
-			});
-			if (user.length > 0 && user[0].email) {
-				res.status(409).json({ error: 'This email has already been taken!' });
-			} else {
-				datastructure.users.push(req.body);
-				var payload = {
-					email: req.body.email
-				};
-				var token = _jsonwebtoken2.default.sign(payload, '1357389', { expiresIn: 60000 });
-				res.setHeader('token', token);
-				res.status(201).json({ message: 'You have successfully signed up!' });
-			}
-		} else {
-			res.status(422).json({ error: 'Please fill in all the fields properly!' });
-		}
-	} else {
+	if (email === ' ' || dob === ' ' || fullName === ' ' || password < 6) {
+		res.status(422).json({ error: 'Please fill in all the fields properly!' });
+	} else if (!email || !dob || !fullName || !password) {
 		res.status(400).json({ error: 'Invalid Request!' });
+	} else if (!datastructure.users) {
+		res.status(500).json({ error: 'Internal Server Error!' });
+	} else {
+		var user = datastructure.users.filter(function (u) {
+			return u.email === email && u.password === password;
+		});
+		if (user.length > 0 && user[0].email) {
+			res.status(409).json({ error: 'This email has already been taken!' });
+		} else {
+			datastructure.users.push(req.body);
+			var payload = {
+				email: req.body.email
+			};
+			var token = _jsonwebtoken2.default.sign(payload, '1357389', { expiresIn: 60000 });
+			res.setHeader('token', token);
+			res.status(201).json({ message: 'You have successfully signed up!' });
+		}
 	}
+	//
+	// if (email && dob && fullName && password) {
+	// 	if (email.length > 0 && dob.length > 0 && fullName.length > 0 && password.length >= 6) {
+	// 		const user = datastructure.users.filter(u => u.email === email && u.password === password);
+	// 		if (user.length > 0 && user[0].email) {
+	// 			res.status(409).json({ error: 'This email has already been taken!' });
+	// 		} else {
+	// 			datastructure.users.push(req.body);
+	// 			const payload = {
+	// 				email: req.body.email,
+	// 			};
+	// 			const token = jwt.sign(payload, '1357389', { expiresIn: 60000 });
+	// 			res.setHeader('token', token);
+	// 			res.status(201).json({ message: 'You have successfully signed up!' });
+	// 		}
+	// 	} else {
+	// 		res.status(422).json({ error: 'Please fill in all the fields properly!' });
+	// 	}
+	// } else {
+	// 	res.status(400).json({ error: 'Invalid Request!' });
+	// }
 });
 
 exports.default = usersRouter;
--- src/routes/usersApi.js
+++ src/routes/usersApi.js
@@ -8,26 +8,47 @@ usersRouter.post('/users', (req, res) => {
 	const {
 		email, password, dob, fullName,
 	} = req.body;
-	if (email && dob && fullName && password) {
-		if (email.length > 0 && dob.length > 0 && fullName.length > 0 && password.length >= 6) {
-			const user = datastructure.users.filter(u => u.email === email && u.password === password);
-			if (user.length > 0 && user[0].email) {
-				res.status(409).json({ error: 'This email has already been taken!' });
-			} else {
-				datastructure.users.push(req.body);
-				const payload = {
-					email: req.body.email,
-				};
-				const token = jwt.sign(payload, '1357389', { expiresIn: 60000 });
-				res.setHeader('token', token);
-				res.status(201).json({ message: 'You have successfully signed up!' });
-			}
-		} else {
-			res.status(422).json({ error: 'Please fill in all the fields properly!' });
-		}
-	} else {
+	if (email === ' ' || dob === ' ' || fullName === ' ' || password < 6) {
+		res.status(422).json({ error: 'Please fill in all the fields properly!' });
+	} else if (!email || !dob || !fullName || !password) {
 		res.status(400).json({ error: 'Invalid Request!' });
+	} else if (!datastructure.users) {
+		res.status(500).json({ error: 'Internal Server Error!' });
+	} else {
+		const user = datastructure.users.filter(u => u.email === email && u.password === password);
+		if (user.length > 0 && user[0].email) {
+			res.status(409).json({ error: 'This email has already been taken!' });
+		} else {
+			datastructure.users.push(req.body);
+			const payload = {
+				email: req.body.email,
+			};
+			const token = jwt.sign(payload, '1357389', { expiresIn: 60000 });
+			res.setHeader('token', token);
+			res.status(201).json({ message: 'You have successfully signed up!' });
+		}
 	}
+	//
+	// if (email && dob && fullName && password) {
+	// 	if (email.length > 0 && dob.length > 0 && fullName.length > 0 && password.length >= 6) {
+	// 		const user = datastructure.users.filter(u => u.email === email && u.password === password);
+	// 		if (user.length > 0 && user[0].email) {
+	// 			res.status(409).json({ error: 'This email has already been taken!' });
+	// 		} else {
+	// 			datastructure.users.push(req.body);
+	// 			const payload = {
+	// 				email: req.body.email,
+	// 			};
+	// 			const token = jwt.sign(payload, '1357389', { expiresIn: 60000 });
+	// 			res.setHeader('token', token);
+	// 			res.status(201).json({ message: 'You have successfully signed up!' });
+	// 		}
+	// 	} else {
+	// 		res.status(422).json({ error: 'Please fill in all the fields properly!' });
+	// 	}
+	// } else {
+	// 	res.status(400).json({ error: 'Invalid Request!' });
+	// }
 });
 
 export default usersRouter;
