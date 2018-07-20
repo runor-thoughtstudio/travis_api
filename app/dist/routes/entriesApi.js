8f044f775989895ea7256ff7b6ec24ef44daae1b
kampkelly <kampkellykeys@gmail.com>
2018-07-20 23:23:43 +0100

    Refactor update entry route

--- app/dist/routes/entriesApi.js
+++ app/dist/routes/entriesApi.js
@@ -30,20 +30,7 @@ entriesRouter.post('/entries', function (req, res) {
 });
 
 entriesRouter.put('/entries/:id', function (req, res) {
-	var datastructure = req.app.get('appData');
-	if (req.body.title === ' ' || req.body.description === ' ') {
-		res.status(422).json({ error: 'Please fill in all the fields properly!' });
-	} else if (!datastructure.entries) {
-		res.status(500).json({ error: 'Internal Server Error!' });
-	} else if (datastructure.entries === undefined || datastructure.entries[req.params.id] === undefined) {
-		res.status(404).json({ error: 'This entry does not exist!' });
-	} else if (req.body.title && req.body.description) {
-		datastructure.entries[req.params.id].title = req.body.title;
-		datastructure.entries[req.params.id].description = req.body.description;
-		res.status(200).json({ message: 'This entry has been updated!' });
-	} else {
-		res.status(400).json({ error: 'Invalid request!' });
-	}
+	Entry.update(req, res);
 });
 
 entriesRouter.delete('/entries/:id', function (req, res) {
